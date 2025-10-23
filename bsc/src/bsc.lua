e370375dae965b12c0edbdf07d69faf853121bf1
ZhangRun <127740919+Water-Run@users.noreply.github.com>
2025-10-24 00:44:11 +0800

    first line of code

---
+++ bsc/src/bsc.lua
@@ -0,0 +1,17 @@
+--[[
+BatScript Compiler
+See: https://github.com/Water-Run/batScript/tree/main
+Doc: https://github.com/Water-Run/batScript/blob/main/README.md
+
+A procedural, strongly-typed scripting language that compiles to .bat, bringing a more modern development experience to Windows batch scripting.
+
+Author |> WaterRun
+File |> bsc.lua
+License |> MIT
+Version |> 0.1
+Time |> 2025-10-24
+
+Compile: use https://github.com/Water-Run/luaToEXE
+]]
+
+__VERSION = "0.1"
